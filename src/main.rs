40014800450889dfac2daf489e6e738eec2264de
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-04-25 13:08:22 -0700

    Add some comments (mostly for me)

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use std::net::SocketAddr;
 mod questions;
 #[tokio::main]
 async fn main() {
+    //Function for route "/"
     let hello = Router::new().route("/", get(say_hello));
 
+    //Base addr
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    //Serve on the addr
     axum::Server::bind(&addr)
         .serve(hello.into_make_service())
         .await
